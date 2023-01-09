18206863a441251b4af64cd25e5ccf37a66218bd
robillard johnny <johnnylaurie@hotmail.fr>
2023-01-09 14:47:30 +0100

    requete

--- exercie9 requete.sql
+++ exercie9 requete.sql
@@ -1,3 +1,7 @@
 --Lister les commandes dont le total est supérieur à 10000€ ;
  --on exclura dans le calcul du total les articles commandés en quantité supérieure ou égale à 1000. 
 
+SELECT numcom,qtecde * priuni 
+FROM ligcom 
+WHERE qtecde<1000
+GROUP BY numcom,qtecde * priuni>10000
