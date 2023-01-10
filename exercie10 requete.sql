f87905147a96a41845dee5954a0a8bf29b7194a6
robillard johnny <johnnylaurie@hotmail.fr>
2023-01-10 09:43:36 +0100

    requete

--- exercie10 requete.sql
+++ exercie10 requete.sql
@@ -1,5 +1,7 @@
 --Lister les commandes par nom de fournisseur.
 --Afficher nom du fournisseur, numéro de commande et date.
 
-
+SELECT numcom,datcom,nomfou
+FROM `entcom`
+INNER JOIN fournis ON entcom.numfou=fournis.numfou;
 
