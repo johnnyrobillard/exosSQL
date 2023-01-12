3569662609c38cc63727541d52b3cf916430e54b
robillard johnny <johnnylaurie@hotmail.fr>
2023-01-12 09:54:24 +0100

    requete

--- exercice13 requete.sql
+++ exercice13 requete.sql
@@ -1,4 +1,5 @@
 --Coder de 2 manières différentes la requête suivante : Lister les commandes dont le fournisseur est celui de la commande n°70210.
 --Afficher numéro de commande et date
-
-
+SELECT numcom,datcom 
+FROM entcom 
+WHERE numfou = 120; 
