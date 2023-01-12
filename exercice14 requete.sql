420c5c1a98c00a1701d4892479959fa817cea1d8
robillard johnny <johnnylaurie@hotmail.fr>
2023-01-12 13:46:41 +0100

    requete

--- exercice14 requete.sql
+++ exercice14 requete.sql
@@ -1,3 +1,7 @@
 --Dans les articles susceptibles d’être vendus, lister les articles moins chers (basés sur Prix1) que le moins cher des rubans (article dont le premier caractère commence par R).
 --Afficher libellé de l’article et prix1
 
+SELECT libart,prix1
+FROM vente
+JOIN produit 
+ON produit.codart=vente.codart;
