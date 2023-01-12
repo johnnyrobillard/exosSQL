ef61b2ddbb2ed3caccd921c6019084b9c3bf4789
robillard johnny <johnnylaurie@hotmail.fr>
2023-01-12 10:24:02 +0100

    requete

---
+++ exercice14 requete.sql
@@ -0,0 +1,3 @@
+--Dans les articles susceptibles d’être vendus, lister les articles moins chers (basés sur Prix1) que le moins cher des rubans (article dont le premier caractère commence par R).
+--Afficher libellé de l’article et prix1
+
