b0e30bc5c27bdcfe88a6ab4b7496ffb661a8de8a
robillard johnny <johnnylaurie@hotmail.fr>
2023-01-13 09:28:32 +0100

    reqtete

--- exercice19 requete.sql
+++ exercice19 requete.sql
@@ -1,2 +1,7 @@
 --Calculer le chiffre d'affaire par fournisseur pour l'année 2018,
 --sachant que les prix indiqués sont hors taxes et que le taux de TVA est 20%.
+SELECT numfou, SUM(qtecde*priuni-(qtecde*priuni)*0.2) 
+FROM entcom 
+JOIN ligcom ON ligcom.numcom=entcom.numcom
+WHERE year(derliv)=2018 
+GROUP BY numfou; 
