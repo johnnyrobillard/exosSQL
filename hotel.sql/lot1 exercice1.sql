b75f830c456f3ef7d492afdc74475ae107eec8b0
robillard johnny <johnnylaurie@hotmail.fr>
2023-01-13 10:38:07 +0100

    requete

---
+++ hotel.sql/lot1 exercice1.sql
@@ -0,0 +1,4 @@
+--Afficher la liste des hôtels.
+--Le résultat doit faire apparaître le nom de l’hôtel et la ville
+SELECT hot_nom 
+FROM hotel; 
---
+++ hotel.sql/lot1 exercice2.sql
@@ -0,0 +1,5 @@
+--Afficher la ville de résidence de Mr White
+--Le résultat doit faire apparaître le nom, le prénom, et l'adresse du client
+SELECT cli_nom,cli_prenom,cli_adresse 
+FROM client 
+WHERE cli_nom ='white'; 
