ce9b9c075e348dc215902e7c6469fef0c99e4f17
robillard johnny <johnnylaurie@hotmail.fr>
2023-01-13 12:19:47 +0100

    requete

--- hotel.sql/lot1 exercice3.sql
+++ hotel.sql/lot1 exercice3.sql
@@ -1,2 +1,5 @@
 --Afficher la liste des stations dont l’altitude < 1000
 --Le résultat doit faire apparaître le nom de la station et l'altitude
+SELECT sta_nom
+FROM station 
+WHERE sta_altitude < 1000; 
