6f3d952a98cb9e83972b6c3e0fb2f0fee016bb63
robillard johnny <johnnylaurie@hotmail.fr>
2023-01-17 11:45:27 +0100

    requete

--- northwind.sql/ercercie2 northwind.sql
+++ northwind.sql/ercercie2 northwind.sql
@@ -1,1 +1,6 @@
 --Liste des produits vendus par le fournisseur "Exotic Liquids" :
+
+SELECT ProductName,UnitPrice 
+FROM products 
+JOIN suppliers ON suppliers.SupplierID = products.SupplierID 
+WHERE Companyname = 'Exotic Liquids'; 
