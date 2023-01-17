403699d5ae4d7176dc909ee2a30ba0822248f89d
robillard johnny <johnnylaurie@hotmail.fr>
2023-01-17 09:09:28 +0100

    requete

--- hotel.sql/lot3 exercice17.sql
+++ hotel.sql/lot3 exercice17.sql
@@ -1,1 +1,7 @@
 --Afficher la durée moyenne des réservations par station
+
+SELECT sta_id,avg(DATEDIFF(res_date_fin, res_date_debut))
+FROM station
+jOIN hotel ON hot_sta_id = sta_id 
+JOIN chambre ON hot_id = cha_hot_id 
+JOIN reservation ON res_cha_id = cha_id; 
