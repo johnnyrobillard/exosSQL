527496e9019d6e11aa1c619bbee50ca9c4e27749
robillard johnny <johnnylaurie@hotmail.fr>
2023-01-20 12:17:38 +0100

    requete

--- northwind.sql/ercercie8 northwind.sql
+++ northwind.sql/ercercie8 northwind.sql
@@ -1,1 +1,7 @@
 --Chiffre d'affaires détaillé par mois, sur les ventes de 1997 :
+
+SELECT MONTH(OrderDate), SUM(UnitPrice*Quantity)
+FROM orders
+JOIN `order details`ON orders.OrderID=`order details`.`OrderID`
+WHERE year(OrderDate)='1997'
+GROUP BY MONTH(OrderDate);
