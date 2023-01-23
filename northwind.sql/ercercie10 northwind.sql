0819ea20026b9f34a8e94258bda0d4f1c027fecf
robillard johnny <johnnylaurie@hotmail.fr>
2023-01-23 16:51:07 +0100

    requete

--- northwind.sql/ercercie10 northwind.sql
+++ northwind.sql/ercercie10 northwind.sql
@@ -1,1 +1,5 @@
 --Quel est le délai moyen de livraison en jours ?
+
+
+SELECT round(AVG(DATEDIFF(ShippedDate, OrderDate)))
+FROM orders; 
--- northwind.sql/ercercie9 northwind.sql
+++ northwind.sql/ercercie9 northwind.sql
@@ -1,1 +1,8 @@
 --A quand remonte la dernière commande du client nommé "Du monde entier" ?
+
+SELECT OrderDate
+FROM customers
+JOIN orders ON orders.CustomerID = customers.CustomerID 
+JOIN `order details`ON `order details`.OrderID=orders.OrderID 
+WHERE CompanyName ='Du monde entier' AND year(OrderDate)=1998 
+GROUP BY OrderDate; 
